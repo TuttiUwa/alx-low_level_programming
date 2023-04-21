94fae7a35faf8750b3e1dbc471c09cdacfd05d8f
Adetutu Uwanikone <adetutu.awofeso@gmail.com>
2023-04-20 21:22:55 -0700

    sizeof correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
- * main - A program that prints the size of various types on the computer it is compiled and run on
+ * main - prints the size of various types on the computer
  * Return: 0(Success)
  */
-
 int main(void)
 {
 printf("Size of a char: %i byte(s)\n", sizeof(char));

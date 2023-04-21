41bfe32c43d787e111e38ecef03a3d00db0ac9d8
Adetutu Uwanikone <adetutu.awofeso@gmail.com>
2023-04-21 15:52:17 -0700

    last digit correction

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - A program that gets the last digit of a number
- * Description - 'get the last digit of a number'
- * Return : 0 (success)
+ *main - A program that gets the last digit of a number
+ *Description - 'get the last digit of a number'
+ *Return : 0 (success)
  */
 int main(void)
 {
@@ -14,12 +14,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	a = n % 10;
-
 	if (a > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, a);
 	}
-
 	else if (a == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, a);

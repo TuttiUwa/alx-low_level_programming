f94d2cf70ad9b0db8c85c106f2ad68f6d7e03c08
Adetutu Uwanikone <adetutu.awofeso@gmail.com>
2023-04-27 16:34:50 -0700

    prints straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - prints a straight line n times
+ *
+ * @n: number of times the straight line is printed
+ * Return: no return
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}

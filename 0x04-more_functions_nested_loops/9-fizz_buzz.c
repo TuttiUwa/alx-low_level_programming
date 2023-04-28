fb55272868cb8bab1da80ed8ed85c5b5db253db5
Adetutu Uwanikone <adetutu.awofeso@gmail.com>
2023-04-27 17:29:54 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,33 +1,32 @@
 #include <stdio.h>
 
 /**
- * main - prints Fizz for 3 , Buszz for 5 and FizzBuss for
- * multiples of 3 and 5
- * Return: Always 0;
+ * main - prints Buzz each numbers of 3 and 5.
+ * Return: Always 0.
  */
 int main(void)
 {
 	int n;
 
 	n = 1;
-	printf("%d ", n);
+	printf("%d", n);
 	for (n = 2; n <= 100; n++)
 	{
 		if ((n % 3 == 0) && (n % 5 == 0))
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		}
 		else if (n % 3 == 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
 		}
 		else if (n % 5 == 0)
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 		}
 		else
 		{
-			printf("%d ", n);
+			printf(" %d", n);
 		}
 	}
 	printf("\n");

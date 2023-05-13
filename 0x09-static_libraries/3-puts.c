666f73d6a0e91dd4a812c932bda7ab588ef262b0
Adetutu Uwanikone <adetutu.awofeso@gmail.com>
2023-05-13 16:30:00 -0700

    creating static files

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+void _puts(char *s)
+{
+	puts(s);
+}
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a mulitlingual puzzle");
+	return (0);
+}
---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: the std function putchar(c)
+ */
+int _putchar(char c)
+{
+	return (putchar(c));
+}

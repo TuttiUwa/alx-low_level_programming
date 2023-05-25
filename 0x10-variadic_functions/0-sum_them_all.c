3650ac3ed5aaee9a9b39bbfd089bc772955173b5
Adetutu Uwanikone <adetutu.awofeso@gmail.com>
2023-05-25 11:57:24 -0700

    sums all arguments

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -6,10 +6,10 @@
  * @n: The number of paramters passed to the function.
  * @...: A variable number of paramters to calculate the sum of.
  *
- * Return: if n == 0 - 0.
- * 	Otherwise - the sume of all parameters.
+ * Return: If n == 0 - 0.
+ *         Otherwise - the sum of all parameters.
  */
-int sum_them_all(const unsigned int n ,...)
+int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i, sum = 0;
